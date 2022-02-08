f4832c47c2fb38f34b531a774b618b4637ff49d1
Joptis <60551514+Joptis@users.noreply.github.com>
2022-02-08 17:33:44 -0300

    Cambio de interfaz y generación bloque IP
    Se logra exportar el codigo de alto niver a un Bloque IP. Se requiere realizar pruebas con el código anterior

--- Euc.cpp
+++ Euc.cpp
@@ -1,30 +1,34 @@
 #include <stdlib.h>     /* abs */
 #include <math.h>
 #include <cmath>
-#define N = 64
-int Euc_res(char in_vec1[64], char in_vec2[64])
+#define N = 8
+int Euc_res(char in_vec1[8], char in_vec2[8])
 {
-	#pragma HLS INTERFACE m_axi port=in_vec1 bundle=gmem0
- 	#pragma HLS INTERFACE m_axi port=in_vec2 bundle=gmem1
-	#pragma HLS INTERFACE m_axi port=Euc_res bundle=gmem0
-	char abs_res[64];
-	char abs_pow[64];
+	#pragma HLS INTERFACE ap_memory port=in_vec1
+ 	#pragma HLS INTERFACE ap_memory port=in_vec2
+	#pragma HLS INTERFACE ap_memory port=return
+	char abs_res[8];
+	char abs_pow[8];
 	//abs_res= abs(in_vec1-in_vec2);
 	int i;
 	int sumatoria=0;
 	int raiz=0;
 	int Euc_out;
-	#pragma HLS array_partition complete variable=abs_pow
 
 
-	//#pragma HLS interface axis depth=64 port=in_vec1
-	//#pragma HLS interface axis depth=64 port=in_vec2
-	//#pragma HLS interface axis depth=64 port=Euc_out
+	//#pragma HLS array_partition complete variable=abs_pow
+	#pragma HLS array_partition variable=in_vec1 complete
+	#pragma HLS array_partition variable=in_vec2 complete
+
+
+	//#pragma HLS interface axis depth=8 port=in_vec1
+	//#pragma HLS interface axis depth=8 port=in_vec2
+	//#pragma HLS interface axis depth=8 port=Euc_out
 
 	//#pragma HLS pipeline
 
 	int delta;
-	for (i=0;i<64;i++)
+	for (i=0;i<8;i++)
 	{
 		//abs_pow[i]= pow(in_vec1[i]-in_vec2[i],2);
 		#pragma HLS PIPELINE II=1
